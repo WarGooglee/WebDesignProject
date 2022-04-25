63020c997bfc461028279178e8aab99ae88bdecd
WarGooglee <s00227303@mail.itsligo.ie>
2022-04-25 06:30:28 +0100

    Final update

--- main.js
+++ main.js
@@ -34,3 +34,40 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
     items[0].classList.add("carousel__item--selected");
     buttons[0].classList.add("carousel__button--selected");
   });
+
+  document.querySelectorAll(".carousel_2").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel__item2");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="carousel__button2"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel__button2");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        // un-select all the items
+        items.forEach((item) =>
+          item.classList.remove("carousel__item--selected2")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("carousel__button--selected2")
+        );
+  
+        items[i].classList.add("carousel__item--selected2");
+        button.classList.add("carousel__button--selected2");
+      });
+    });
+  
+    // Select the first item on page load
+    items[0].classList.add("carousel__item--selected2");
+    buttons[0].classList.add("carousel__button--selected2");
+  });
